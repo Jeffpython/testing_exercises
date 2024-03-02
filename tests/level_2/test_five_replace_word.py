cdc200bfe7c55b498d2af3d58d5781a661781cfa
Jeff <jeffwingchun@mail.ru>
2024-03-02 23:26:33 +0300

    level_2_refactoring

--- tests/level_2/test_five_replace_word.py
+++ tests/level_2/test_five_replace_word.py
@@ -2,64 +2,19 @@ import pytest
 from functions.level_2.five_replace_word import replace_word
 
 
-def test__replace_word__one_word_from_text_contains_in_replace_word():
-    text = 'old'
-    replace_from = 'old'
-    replace_to = 'new'
-
-    expected_result = replace_word(text, replace_from, replace_to)
-    assert expected_result == 'new'
-
-
-def test__replace_word__two_words_from_text_contains_in_replace_word():
-    text = 'old old'
-    replace_from = 'old'
-    replace_to = 'new'
-
-    expected_result = replace_word(text, replace_from, replace_to)
-    assert expected_result == 'new new'
+@pytest.mark.parametrize(('text', 'replace_from', 'replace_to', 'expected_result'), [
+    ('old old', 'old', 'new', 'new new'),
+    ('OLD OLD', 'old', 'new', 'new new'),
+    ('old', 'old', '', ''),
+])
+def test__replace_word__replace_all_words(text, replace_from, replace_to, expected_result):
+    assert replace_word(text, replace_from, replace_to) == expected_result
 
 
-def test__replace_word__upper_word_from_text_contains_in_replace_word():
-    text = 'OLD OLD'
-    replace_from = 'old'
-    replace_to = 'new'
-
-    expected_result = replace_word(text, replace_from, replace_to)
-    assert expected_result == 'new new'
-
-
-def test__replace_word__word_from_text_not_contains_in_replace_word():
-    text = 'old'
-    replace_from = 'other'
-    replace_to = 'new'
-
-    expected_result = replace_word(text, replace_from, replace_to)
-    assert expected_result == 'old'
-
-
-def test__replace_word__text_is_blank():
-    text = ''
-    replace_from = 'old'
-    replace_to = 'new'
-
-    expected_result = replace_word(text, replace_from, replace_to)
-    assert expected_result == ''
-
-
-def test__replace_word__replace_from_is_blank():
-    text = 'old'
-    replace_from = ''
-    replace_to = 'new'
-
-    expected_result = replace_word(text, replace_from, replace_to)
-    assert expected_result == 'old'
-
-
-def test__replace_word__replace_to_is_blank():
-    text = 'old'
-    replace_from = 'old'
-    replace_to = ''
-
-    expected_result = replace_word(text, replace_from, replace_to)
-    assert expected_result == ''
+@pytest.mark.parametrize(('text', 'replace_from', 'replace_to', 'expected_result'), [
+    ('old', 'other', 'new', 'old'),
+    ('', 'old', 'new', ''),
+    ('old', '', 'new', 'old'),
+])
+def test__replace_word__do_not_replace_words(text, replace_from, replace_to, expected_result):
+    assert replace_word(text, replace_from, replace_to) == expected_result
--- tests/level_2/test_two_square_equation.py
+++ tests/level_2/test_two_square_equation.py
@@ -4,46 +4,21 @@ from functions.level_2.two_square_equation import solve_square_equation
 
 
 def test__solve_square_equation__discriminant_greater_than_zero():
-    square_coefficient = 1
-    linear_coefficient = 2
-    const_coefficient = -3
-
-    expected_result = solve_square_equation(square_coefficient, linear_coefficient, const_coefficient)
-    assert expected_result == (-3.0, 1.0)
+    assert solve_square_equation(1, 2, -3) == (-3.0, 1.0)
 
 
 @pytest.mark.xfail(reason="there must be one root")
 def test__solve_square_equation__discriminant_equal_zero():
-    square_coefficient = 1
-    linear_coefficient = -6
-    const_coefficient = 9
-
-    expected_result = solve_square_equation(square_coefficient, linear_coefficient, const_coefficient)
-    assert expected_result == (3.0, None)
+    assert solve_square_equation(1, -6, 9) == (3.0, None)
 
 
 def test__solve_square_equation__discriminant_less_than_zero():
-    square_coefficient = 1
-    linear_coefficient = 2
-    const_coefficient = 2
-
-    expected_result = solve_square_equation(square_coefficient, linear_coefficient, const_coefficient)
-    assert expected_result == (None, None)
+    assert solve_square_equation(1, 2, 2) == (None, None)
 
 
 def test__solve_square_equation__square_coefficient_equal_zero():
-    square_coefficient = 0
-    linear_coefficient = 2
-    const_coefficient = -3
-
-    expected_result = solve_square_equation(square_coefficient, linear_coefficient, const_coefficient)
-    assert expected_result == (1.5, None)
+    assert solve_square_equation(0, 2, -3) == (1.5, None)
 
 
 def test__solve_square_equation__square_coefficient_and_linear_coefficient_equal_zero():
-    square_coefficient = 0
-    linear_coefficient = 0
-    const_coefficient = -3
-
-    expected_result = solve_square_equation(square_coefficient, linear_coefficient, const_coefficient)
-    assert expected_result == (None, None)
+    assert solve_square_equation(0, 0, -3) == (None, None)
