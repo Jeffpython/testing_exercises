b4b33ae75870c75b1d696c86b752e3a8b1e3f010
Jeff <jeffwingchun@mail.ru>
2024-03-10 16:11:01 +0300

    replacing xpass with xfail

--- tests/level_2/test_one_pr_url.py
+++ tests/level_2/test_one_pr_url.py
@@ -7,13 +7,12 @@ def test__is_github_pull_request_url__correct_link():
     assert is_github_pull_request_url(url)
 
 
-@pytest.mark.xfail(reason="this link can be taken as correct")
 def test__is_github_pull_request_url__link_without_protocol():
     url = 'github.com/Jeffpython/testing_exercises/pull/1'
-    assert is_github_pull_request_url(url)
+    assert is_github_pull_request_url(url) is False
 
 
-@pytest.mark.xpass(reason="the link is not correct")
+@pytest.mark.xfail(reason="the protocol is not correct")
 def test__is_github_pull_request_url__link_with_incorrect_protocol():
     url = 'ftp://github.com/Jeffpython/testing_exercises/pull/1'
     assert is_github_pull_request_url(url)
@@ -24,18 +23,17 @@ def test__is_github_pull_request_url__link_with_incorrect_domain_name():
     assert not is_github_pull_request_url(url)
 
 
-@pytest.mark.xpass(reason="pull number is required")
+@pytest.mark.xfail(reason="pull number is required")
 def test__is_github_pull_request_url__link_without_pull_number():
     url = 'https://github.com/Jeffpython/testing_exercises/pull/'
     assert is_github_pull_request_url(url)
 
 
-@pytest.mark.xpass(reason="the pull number must be correct")
+@pytest.mark.xfail(reason="the pull number must be correct")
 def test__is_github_pull_request_url__link_with_incorrect_pull_number():
     url = 'https://github.com/Jeffpython/testing_exercises/pull/a'
     assert is_github_pull_request_url(url)
 
 
 def test__is_github_pull_request_url__blank_string():
-    url = ''
-    assert not is_github_pull_request_url(url)
+    assert not is_github_pull_request_url('')
