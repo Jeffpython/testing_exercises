25252c87559cd70fb3e80f3f2d72dfe2826c2882
Jeff <jeffwingchun@mail.ru>
2024-04-23 21:05:43 +0300

    refactoring

--- tests/level_4/test_four_lines_counter.py
+++ tests/level_4/test_four_lines_counter.py
@@ -16,15 +16,12 @@ def test__count_lines_in__returns_none_if_the_file_does_not_exist(isfile_mock):
     ('1', 1),
     ('1\n2', 2)
 ])
-def test__count_lines_in__returns_number_of_lines(create_file, content, expected):
-    filepath = create_file(content=content)
+def test__count_lines_in__returns_number_of_lines(make_file, content, expected):
+    filepath = make_file(content=content)
     assert count_lines_in(filepath) == expected
 
 
-@pytest.mark.parametrize(('content', 'expected'), [
-    ('#', 0),
-    (' #', 0)
-])
-def test__count_lines_in__line_is_not_counted_if_it_starts_with_number_sign(create_file, content, expected):
-    filepath = create_file(content=content)
-    assert count_lines_in(filepath) == expected
+@pytest.mark.parametrize('content', ['#', ' #'])
+def test__count_lines_in__line_is_not_counted_if_it_starts_with_number_sign(make_file, content):
+    filepath = make_file(content=content)
+    assert count_lines_in(filepath) == 0
